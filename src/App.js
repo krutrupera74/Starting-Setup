de7599fd2607202dccfcaec206c78862cb125473
krutr <krutrupera74@gmail.com>
2023-01-10 10:02:13 +0530

    initial checkin

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState, useCallback } from 'react';
+import Button from './components/UI/Button/Button';
+import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
+
+function App() {
+
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const toggleParagraphHandler = useCallback(() => {
+    setShowParagraph(prevShowParagraph => !prevShowParagraph);
+  }, []);
+
+  console.log('App is running');
+
+
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
+    </div>
+  );
+}
+
+export default App;
